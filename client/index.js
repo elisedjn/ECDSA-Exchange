f3a8fa1a7d5a415bd08b192c2f79ba86cd23b732
Elise Djn <elisedjn@protonmail.com>
2022-05-01 20:18:21 +0200

    done

--- client/index.js
+++ client/index.js
@@ -1,34 +1,133 @@
-import "./index.scss";
+import './index.scss';
+import SHA256 from 'crypto-js/sha256';
+import * as secp from '@noble/secp256k1';
 
-const server = "http://localhost:3042";
+const server = 'http://localhost:3042';
 
-document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
-  if(value === "") {
-    document.getElementById("balance").innerHTML = 0;
+const fetchBalance = address => {
+  if (address === '') {
+    document.getElementById('balance').innerHTML = 0;
     return;
   }
 
-  fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+  fetch(`${server}/balance/${address}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(({ balance }) => {
+      document.getElementById('balance').innerHTML = balance;
+    });
+};
+
+const hasErrors = (privateKey, amount, recipient) => {
+  const errorMessage = document.getElementById('error-message');
+
+  if (privateKey.length !== 64) {
+    errorMessage.style.display = 'block';
+    errorMessage.innerHTML = 'ERROR : Invalid private key';
+    return true;
+  }
+  if (!amount) {
+    errorMessage.style.display = 'block';
+    errorMessage.innerHTML = 'ERROR : Please select an amount to transfer';
+    return true;
+  }
+  if (!recipient) {
+    errorMessage.style.display = 'block';
+    errorMessage.innerHTML =
+      'ERROR : Please enter a recipient for this transfer';
+    return true;
+  }
+
+  errorMessage.style.display = 'none';
+  errorMessage.innerHTML = '';
+  return false;
+};
+
+const handleSuccess = (balance, publicAddress, amount, recipient) => {
+  document.getElementById('balance').innerHTML = balance;
+  document.getElementById('exchange-address').value = publicAddress;
+  document.getElementById('recipient').value = '';
+  document.getElementById('transfer-amount').value = null;
+  document.getElementById('success-message').style.display = 'block';
+  document.getElementById('success-amount').innerHTML = amount;
+  document.getElementById('success-recipient').innerHTML = recipient;
+};
+
+document
+  .getElementById('exchange-address')
+  .addEventListener('input', ({ target: { value } }) => {
+    fetchBalance(value);
   });
-});
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+document
+  .getElementById('private-key')
+  .addEventListener('input', ({ target: { value } }) => {
+    if (value.length === 64) {
+      const errorMessage = document.getElementById('error-message');
+      errorMessage.style.display = 'none';
+      errorMessage.innerHTML = '';
+      const privateKey = value.toLowerCase();
+      const publicKey = Buffer.from(secp.getPublicKey(privateKey)).toString(
+        'hex'
+      );
+      const publicAddress = '0x' + publicKey.slice(publicKey.length - 40);
 
-  const body = JSON.stringify({
-    sender, amount, recipient
+      document.getElementById('public-address-wrapper').style.display = 'block';
+      document.getElementById('public-address').innerHTML = publicAddress;
+      document.getElementById('exchange-address').value = publicAddress;
+      fetchBalance(publicAddress);
+    } else {
+      document.getElementById('public-address-wrapper').style.display = 'none';
+      document.getElementById('public-address').innerHTML = '';
+    }
   });
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+document
+  .getElementById('transfer-amount')
+  .addEventListener('click', async () => {
+    const amount = document.getElementById('send-amount')?.value;
+    const recipient = document
+      .getElementById('recipient')
+      ?.value?.toLowerCase();
+    const privateKey = document
+      .getElementById('private-key')
+      ?.value?.toLowerCase();
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+    if (hasErrors(privateKey, amount, recipient)) {
+      return;
+    }
+
+    const message = {
+      amount,
+      recipient
+    };
+
+    const msgHash = SHA256(JSON.stringify(message));
+
+    const signature = Buffer.from(
+      await secp.sign(msgHash.toString(), privateKey)
+    ).toString('hex');
+
+    const publicKey = Buffer.from(secp.getPublicKey(privateKey)).toString(
+      'hex'
+    );
+
+    const body = JSON.stringify({ signature, message, publicKey });
+
+    const request = new Request(`${server}/send`, { method: 'POST', body });
+
+    fetch(request, { headers: { 'Content-Type': 'application/json' } })
+      .then(response => {
+        return response.json();
+      })
+      .then(({ balance, publicAddress, error }) => {
+        if (error) {
+          const errorMessage = document.getElementById('error-message');
+          errorMessage.style.display = 'block';
+          errorMessage.innerHTML = 'ERROR : ' + error;
+        } else {
+          handleSuccess(balance, publicAddress, amount, recipient);
+        }
+      });
   });
-});
--- server/index.js
+++ server/index.js
@@ -2,31 +2,80 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const secp = require('@noble/secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
+const generateOneAddressPair = () => {
+  const privateKey = Buffer.from(secp.utils.randomPrivateKey()).toString('hex');
+  const publicKey = Buffer.from(secp.getPublicKey(privateKey)).toString('hex');
+  const publicAddress = '0x' + publicKey.slice(publicKey.length - 40);
+  return { privateKey, publicAddress };
+};
+
+const generateAddresses = nb => {
+  const addresses = [];
+  for (let i = 0; i < nb; i++) {
+    const newAddress = generateOneAddressPair();
+    addresses.push(newAddress);
+  }
+
+  return addresses;
+};
+
+const addresses = generateAddresses(3);
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+  [addresses[0].publicAddress]: 100,
+  [addresses[1].publicAddress]: 50,
+  [addresses[2].publicAddress]: 75
+};
 
 app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+  const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const { message, signature, publicKey } = req.body;
+  const msgHash = SHA256(JSON.stringify(message)).toString();
+  if (secp.verify(signature, msgHash, publicKey)) {
+    const { amount, recipient } = message;
+    const sender = '0x' + publicKey.slice(publicKey.length - 40);
+    if (!balances[sender]) {
+      res.send({ error: 'Sender not found' });
+      return;
+    }
+    if (balances[sender] >= amount) {
+      balances[sender] -= amount;
+      balances[recipient] = (balances[recipient] || 0) + +amount;
+      res.send({ balance: balances[sender], publicAddress: sender });
+      console.log(`From : ${sender} to : ${recipient} amount : ${amount}ETH`);
+    } else {
+      res.send({ error: 'Not enough funds' });
+    }
+  } else {
+    res.send({ error: 'Invalid signature' });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log('   ');
+  console.log('Available Accounts');
+  console.log('==========================');
+  Object.keys(balances).forEach((publicAddress, index) => {
+    console.log(`(${index}) ${publicAddress} (${balances[publicAddress]}ETH)`);
+  });
+  console.log('   ');
+  console.log('Private Keys');
+  console.log('==========================');
+  addresses.forEach((address, index) => {
+    console.log(`(${index}) ${address.privateKey}`);
+  });
 });
